394d3f561e7d406f2e6e60e20c79d9678377657b
kai_glo <kai_glo@qq.com>
2020-08-16 11:15:06 +0800

    把本地测试工程提交

---
+++ src/com/kai/git/test.java
@@ -0,0 +1,16 @@
+package com.kai.git;
+
+/**
+ * @author ：renkai
+ * @date ：Created in 2020/8/16 11:08
+ * @description：
+ * @version: 1.0
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello test github");
+        System.out.println("hello test github");
+        System.out.println("hello test github");
+    }
+}
